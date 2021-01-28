a9dd55fd304372eaf4c9bf806c9053bbd5eb4e77
Yerkebulan Kambarov <44515882+kambarovee@users.noreply.github.com>
2021-01-28 16:18:15 +0600

    Added questions

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -3,9 +3,13 @@ import 'Questions.dart';
 class Quiz {
   int _questionNumber = 0;
   List<Questions> _questionsList = [
-    Questions(q: 'First Question', a: true),
-    Questions(q: 'Second Question', a: false),
-    Questions(q: 'Third Question', a: true),
+    Questions(q: 'The currency of Kazakhstan is Tenge', a: true),
+    Questions(q: 'The highest mountain in England is Ben Nevis', a: false),
+    Questions(q: 'Ben Nevis is the tallest mountain in the UK', a: true),
+    Questions(q: 'The capital of Libya is Tripoli', a: true),
+    Questions(q: 'The UK is about the same size as France', a: false),
+    Questions(q: 'The UK is made up of more than 6000 islands', a: true),
+    Questions(q: 'Glasgow is north of Belfast', a: true),
   ];
 
   void nextQuestionNum() {
